07b19fe49dd1fb5743e0fa5c7a2b0d00833a9ece
Emmanuel Nyondo <Emmanuel.Nyondo@righttocare.org>
2026-01-13 15:58:08 +0200

    fix: update 404 page with an extra back button and image scale on mobile

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -12,27 +12,34 @@ const NotFoundPage: React.FC = () => {
         onClick={() => navigate(-1)}
         className="absolute top-6 left-6 flex items-center gap-2 px-4 py-2 bg-gray-200 hover:cursor-pointer rounded hover:bg-gray-300 transition"
       >
-        <ArrowLeft size={18} />
+        <ArrowLeft size={16} />
         Back
       </button>
 
       <img
         src="/img/404Error.png"
         alt="404 Not Found"
-        className="w-[60%] max-w-full mb-6 filter hue-rotate-0 saturate-80"
+        className="w-full lg:w-[55%] max-w-full mb-6 filter hue-rotate-0 saturate-80 "
       />
 
       <h1 className="text-3xl font-bold text-gray-800 mb-2">Oops! Page Not Found</h1>
       <p className="text-gray-600 mb-4">
         The page you are looking for doesn’t exist, is under maintenance or development or has been moved.
       </p>
-
-      <button
-        onClick={() => navigate("/app/dashboard")}
-        className="px-6 py-2 bg-emerald-500 text-white rounded hover:cursor-pointer hover:bg-emerald-700 transition"
-      >
-        Go Back to Dashboard
-      </button>
+      <div className="flex gap-4">
+        <button
+          onClick={() => navigate(-1)}
+          className="px-6 py-2 bg-gray-300 text-gray-800 rounded hover:cursor-pointer hover:bg-gray-400 transition mr-4"
+        >
+          <ArrowLeft size={16} /> Back
+        </button>
+        <button
+          onClick={() => navigate("/app/dashboard")}
+          className="px-6 py-2 bg-emerald-500 text-white rounded hover:cursor-pointer hover:bg-emerald-700 transition"
+        >
+          Back to Dashboard
+        </button>
+      </div>
     </div>
   );
 };
